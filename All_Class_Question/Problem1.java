9a237eeffd195d89dd560ce12358e88dab800404
Ankit vishwakarma <ankitvishwakarma4591@gmail.com>
2024-09-13 03:03:50 +0530

    Class task

---
+++ All_Class_Question/Problem1.java
@@ -0,0 +1,17 @@
+// WAP to print table of 5 using array
+import java.util.*;
+public class Problem1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter table of five :");
+        int table[] = new int[10];
+        for(int i=0;i<10;i++){
+            System.out.print("5 * "+(i+1)+" = ");
+            table[i] = sc.nextInt();
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(table[i]);
+        }
+        sc.close();
+    }
+}
---
+++ All_Class_Question/Problem2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array");
+        int size = sc.nextInt();
+        int array[] = new int[size];
+        for(int i=0;i<size;i++){
+            System.out.print("Enter "+(i+1)+" number :");
+            array[i] = sc.nextInt();
+        }
+        int max = array[0];
+        int min = array[0];
+        for(int i=0;i<size;i++){
+            if(array[i] > max){
+                max = array[i];
+            }else if(array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("Largest number in array is  :"+ max);
+        System.out.println("Smallest number in array is  :"+ min);
+        sc.close();
+    }
+}
